24c88369a341e985a15cf53209c0017d22306d66
MoisesK <moises979@gmail.com>
2022-11-05 18:50:45 -0300

    feat: Isolating footer and header views

--- resources/views/components/layout.blade.php
+++ resources/views/components/layout.blade.php
@@ -12,27 +12,14 @@
 
 <body class="bg-dark text-light">
     <div class="container">
-        <div class="jumbotron bg-warning p-3 my-3">
-
-            <h1 class="text-center">ENTREGAS 2000 - v.2</h1>
-            <p class="text-center fs-5 fst-italic">O SEU SISTEMA DE ENTREGAS RÁPIDAS - FRAMEWORK VERSION</p>
-            <ul class=" nav nav-pills nav-justified navbar-dark bg-dark">
-                <li class="nav-item"><a href="" class="nav-link fs-5 fst-italic">HOME</a></li>
-                <li class="nav-item"><a href="#" class="nav-link fs-5 fst-italic">NOVA ENTREGA</a></li>
-                <li class="nav-item"><a href="#" class="nav-link fs-5 fst-italic ">SOBRE</a></li>
-            </ul>
-
-        </div>
+       <x-header/>
 
         <h1>{{ $title }}</h1>
+        
         {{$slot}}
 
 </body>
 
-<footer class="text-center mb-3">
-    <hr>
-    <a href="https://github.com/MoisesK/" class=""><i class="bi bi-github"></i></a>
-    Moisés Dev - 2022
-</footer>
+<x-footer/>
 
 </html>
