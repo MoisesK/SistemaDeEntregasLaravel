2e7b18b20cf5827d4a427d3b4d6cb67d23fd0bc1
MoisesK <moises979@gmail.com>
2022-11-09 12:36:12 -0300

    refactor: foreignIdFor > Relationship migrations

--- database/migrations/2022_11_06_223701_create_deliveries_table.php
+++ database/migrations/2022_11_06_223701_create_deliveries_table.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Models\DeliveryMan;
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
@@ -15,12 +16,12 @@ return new class extends Migration
     {
         Schema::create('deliveries', function (Blueprint $table) {
             $table->id();
+            $table->foreignIdFor(DeliveryMan::class, 'deliverie_mens_id')->constrained();
             $table->string('title', 100);
             $table->dateTime('deadline');
             $table->string('descript', 200);
             $table->string('stats', 100);
             $table->string('place', 200);
-            $table->foreignId('deliverie_mens_id')->constrained();
             $table->timestamps();
         });
     }
--- database/migrations/2022_11_08_150307_create_delivery_mens_table.php
+++ database/migrations/2022_11_08_150307_create_delivery_mens_table.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Models\Delivery;
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
@@ -15,7 +16,7 @@ return new class extends Migration
     {
         Schema::create('delivery_mens', function (Blueprint $table) {
             $table->id();
-            $table->foreignId('deliveries_id')->constrained();
+            $table->foreignIdFor(Delivery::class, 'deliveries_id')->constrained();
             $table->string('name', 100);
             $table->string('adress', 100);
             $table->string('vehicle', 100);
